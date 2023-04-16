6b734062d1e9bc7beaff6b8f7dc0ac41a17c3f21
devonfox <devonhouston.fox@gmail.com>
2023-04-15 22:55:07 -0700

    count first attempt

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,33 @@
-// Add your code here
+const input = document.querySelector("input");
 
-input.addEventListener('keydown', handleKeyDown);
+input.addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(event) {
+  const inputString = event.target.value.trim();
+  if (event.key == "Enter") {
+    if (inputString !== "") {
+      const ipsum = document.getElementById("ipsum");
+
+      const search = ipsum.textContent.split(/\s+/);
+
+      // Functional methods are so cool!
+      const highlighted = search.map((check) => {
+        if (check === inputString) {
+          // Doesn't quite match style. Look into bootstrap colors.
+          return `<text style="background-color: #ffff00">${check}</text>`;
+        }
+        return check;
+      });
+
+      ipsum.innerHTML = highlighted.join(" ");
+    }
+    else {
+        const ipsum = document.getElementById("ipsum");
+        const search = ipsum.textContent.split(/\s+/);
+        const highlighted = search.map((check) => {
+            return check;
+          });
+          ipsum.innerHTML = highlighted.join(" ");
+    }
+  }
+}
