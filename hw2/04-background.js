ed0733638927782ae4df3d5f00f707dbd0cb909e
devonfox <devonhouston.fox@gmail.com>
2023-04-16 18:26:58 -0700

    added 04 - background

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,41 @@
-// Add your code here
+const interval = document.getElementById('interval');
+const button = document.getElementById('toggle');
+let buttonState = false;
+
+addEventListener('load', () => {
+  // loading random background color on page load
+  randomColor();
+});
+
+// creating a setInterval id per MDN docs
+let id;
+
+button.addEventListener('click', (event) => {
+  event.preventDefault(); // not sure if this necessary? todo: ask prof
+
+  // on click change buttonState
+  buttonState = !buttonState;
+  button.classList.toggle('btn-primary', !buttonState);
+  button.classList.toggle('btn-danger', buttonState);
+  button.value = buttonState ? 'Stop' : 'Start';
+
+  /* if nothing set, set interval to 3 seconds, and
+       multiply value in input by 1000 (for milliseconds) */
+  if (buttonState) {
+    let intervalAmount = interval.value == '' ? 3000 : interval.value * 1000;
+    randomColor();
+    id = setInterval(randomColor, intervalAmount);
+  } else {
+    // clearing setInterval calls
+    clearInterval(id);
+  }
+});
+
+const randomColor = function determineRandomBackgroundColor() {
+  // Using hsla, and dimming alpha value per instruction pdf
+  document.body.style.backgroundColor = `hsla(${Math.floor(
+    Math.random() * 360,
+  )}, ${Math.floor(Math.random() * 50) + 20}%, ${
+    Math.floor(Math.random() * 50) + 20
+  }% ,${Math.random() * 0.8})`;
+};
