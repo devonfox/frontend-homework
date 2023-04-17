3e305ab516a7f3d07b78210a476b2bccbf0534d6
devonfox <devonhouston.fox@gmail.com>
2023-04-16 23:42:57 -0700

    form first draft

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,7 @@
-// Add your code here
+const form = document.getElementById('form-exercise');
+
+//console.logs on submit. is this right?
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log(event);
+});
