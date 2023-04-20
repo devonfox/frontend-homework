bc555d940ca6153b39002c399d23b0bd2dce240e
devonfox <devonhouston.fox@gmail.com>
2023-04-19 17:00:34 -0700

    working on form submission console logging

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,7 +1,21 @@
 const form = document.getElementById('form-exercise');
 
-//console.logs on submit. is this right?
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  console.log(event);
-});
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  let name = document.querySelector("#name");
+  let email = document.querySelector("#email");;
+  let date = document.querySelector("#date");
+  let nosub = "no submission";
+
+  if (!name.value && !email.value) {
+    console.warn("You must enter some data to submit this form");
+  } else {
+    console.group("========= Form Submission =========");
+    name.value ? console.log("Username: ", name.value) : console.log("Username: " + nosub);
+    email.value ? console.log("Email: ", email.value) : console.log("Email: " + nosub);
+    }
+
+    console.groupEnd();
+    event.preventDefault();
+  }
