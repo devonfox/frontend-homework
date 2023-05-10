980d50879a54ae8d3f90059f5f58a7f287ed4c8b
devonfox <devonhouston.fox@gmail.com>
2023-05-10 12:40:58 -0700

    ex1 + 2

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,55 @@
-// url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
+const section = document.getElementById('thrones');
 
-fetch(url);
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const characters = data.map(character => ({
+      image: character.imageUrl,
+      name: character.fullName,
+      title: character.title
+    }));
+    addCards(characters);
+  });
+
+function addCards(characters) {
+  characters.forEach(character => {
+    const card = createCard(character);
+    section.appendChild(card);
+  });
+}
+
+function createCard(character) {
+  const card = document.createElement('div');
+  card.classList.add('card', 'text-center', 'border-0');
+
+  const cardBackground = document.createElement('div');
+  cardBackground.classList.add('card-bg');
+  card.appendChild(cardBackground);
+
+  const image = document.createElement('img');
+  image.src = character.image;
+  image.alt = character.name;
+  image.classList.add('card-img-top', 'portrait', 'mx-auto', 'mt-4');
+  card.appendChild(image);
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body', 'p-2');
+
+  const name = document.createElement('h5');
+  name.classList.add('card-title', 'fw-bold', 'fs-5', 'mx-auto');
+  name.textContent = character.name;
+  cardBody.appendChild(name);
+
+  const title = document.createElement('p');
+  title.classList.add('card-text', 'fw-bold', 'mx-auto');
+  title.textContent = character.title;
+  cardBody.appendChild(title);
+
+  const cardHighlight = document.createElement('div');
+  cardHighlight.classList.add('my-2')
+  cardHighlight.appendChild(cardBody);
+
+  card.appendChild(cardHighlight);
+  return card;
+}
--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -1,5 +1,5 @@
 const backgroundColors = [
-  'rgba(54, 162, 235, 0.8)',
+  'rgba(64, 162, 235, 0.8)',
   'rgba(255, 206, 86, 0.8)',
   'rgba(255, 99, 132, 0.8)',
   'rgba(75, 192, 192, 0.8)',
@@ -13,33 +13,59 @@ const backgroundColors = [
 ];
 
 const borderColors = [
-  'rgba(54, 162, 235, 1)',
-  'rgba(255, 206, 86, 1)',
-  'rgba(255, 99, 132, 1)',
-  'rgba(75, 192, 192, 1)',
-  'rgba(153, 102, 255, 1)',
-  'rgba(255, 159, 64, 1)',
-  'rgba(159, 159, 159, 1)',
-  'rgba(83, 102, 255, 1)',
-  'rgba(40, 159, 64, 1)',
-  'rgba(210, 199, 199, 1)',
-  'rgba(78, 52, 199, 1)',
+  'rgba(64, 162, 235, 0.8)',
+  'rgba(255, 206, 86, 0.8)',
+  'rgba(255, 99, 132, 0.8)',
+  'rgba(75, 192, 192, 0.8)',
+  'rgba(153, 102, 255, 0.8)',
+  'rgba(255, 159, 64, 0.8)',
+  'rgba(199, 199, 199, 0.8)',
+  'rgba(83, 102, 255, 0.8)',
+  'rgba(40, 159, 64, 0.8)',
+  'rgba(210, 199, 199, 0.8)',
+  'rgba(78, 52, 199, 0.8)',
 ];
 
-// url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
 
-const renderChart = () => {
+const renderChart = async () => {
   const donutChart = document.querySelector('.donut-chart');
+
+  const response = await fetch(url);
+  const characters = await response.json();
+  const houses = {};
+  characters.forEach((character) => {
+    if (character.family.includes('House')) {
+      if (houses[character.family]) {
+        houses[character.family] += 1;
+      } else {
+        // creating the key as we loop through
+        houses[character.family] = 1;
+      }
+    }
+  });
+
+  // second pass to make sure houses all exist and THEN add if we find a partial match
+  characters.forEach((character) => {
+    if (!character.family.includes('House')) {
+      for (const key in houses) {
+        if (key.includes(character.family)) {
+          houses[key] += 1;
+        }
+      }
+    }
+  });
+
+  const [labels, characterCount] = [Object.keys(houses), Object.values(houses)];
 
   new Chart(donutChart, {
     type: 'doughnut',
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      labels: labels,
       datasets: [
         {
-          label: 'My First Dataset',
-          data: [1, 12, 33, 5],
+          label: 'House Counts',
+          data: characterCount,
           backgroundColor: backgroundColors,
           borderColor: borderColors,
           borderWidth: 1,
